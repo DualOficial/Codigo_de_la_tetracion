798836c40ea920b5c8f5a7eab75b221d099b65d8
Dual <codifire24@protonmail.com>
2026-01-24 06:56:33 -0600

    Create Math.hpp

---
+++ Math.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include"ortogonals.hpp"
+#include"ortogonalper.hpp"
+#include"TestFunc.hpp"
+#include"permutation.hpp"
+#include"other_permutations.hpp"
+#include"Language.hpp"
+#include"tetration.hpp"
+#include"kuznetsov.hpp"
+#include"dual.hpp"
