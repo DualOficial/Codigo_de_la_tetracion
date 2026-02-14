913ff9fcd1003a545b3eb74393ddf33d0849979f
Dual <codifire24@protonmail.com>
2026-02-14 11:47:00 -0600

    Update TestFunc.hpp

--- TestFunc.hpp
+++ TestFunc.hpp
@@ -6,6 +6,6 @@ template< typename F >
 void TestFunc( F && f , long double init , long double increment );
 
 template< typename F >
-void TestFuncFinite(  F && f , long double init , long double increment , long double max );
+void TestFuncFinite( F && f , long double init , long double increment , long double max );
 
 #include"TestFunc.inl"
