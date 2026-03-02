2cca89666ea136b06c32c4c093f9ba82548b6be8
Dual <codifire24@protonmail.com>
2026-03-01 19:40:38 -0600

    Delete other_permutations.cpp

--- other_permutations.cpp
+++
@@ -1,7 +0,0 @@
-#include"other_permutations.hpp"
-#include"permutation.hpp"
-
-int plus_tetration_complexity = 10000;
-int minus_tetration_complexity = 10000;
-int hyper_square_complexity = 10000;
-int natural_tetration_complexity = 10000;
